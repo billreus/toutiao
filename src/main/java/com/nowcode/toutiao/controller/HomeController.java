2aa7f99907a0f1d6beb36a37df8f9d395f92dda5
billreus <rine328909144@gmail.com>
2018-12-21 23:24:46 +0800

    继续= =

--- src/main/java/com/nowcode/toutiao/controller/HomeController.java
+++ src/main/java/com/nowcode/toutiao/controller/HomeController.java
@@ -41,7 +41,7 @@ public class HomeController {
 
     @RequestMapping(path = {"/", "/index"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String index(Model model) {
-        model.addAttribute("vos", getNews(0, 0, 10));
+        model.addAttribute("vos", getNews(2, 0, 10));
         return "home";
     }
 
