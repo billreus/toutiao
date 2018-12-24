b3c3142cc7bbef192e4d48f3a73904b5f8a30773
billreus <rine328909144@gmail.com>
2018-12-24 22:57:19 +0800

    新增用户注册，不过主页右上角缺少登陆页面，需要调试前端

---
+++ src/main/java/com/nowcode/toutiao/controller/LoginController.java
@@ -0,0 +1,58 @@
+package com.nowcode.toutiao.controller;
+
+
+import com.nowcode.toutiao.model.News;
+import com.nowcode.toutiao.model.ViewObject;
+import com.nowcode.toutiao.service.NewsService;
+import com.nowcode.toutiao.service.UserService;
+import com.nowcode.toutiao.util.ToutiaoUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = {"/reg"}, method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String reg(Model model, @RequestParam("username") String username,
+                      @RequestParam("password") String password,
+                      @RequestParam(value="rember", defaultValue = "0") int rememberme) {
+        try {
+            Map<String, Object> map = userService.register(username, password);
+/*
+            if (map.containsKey("ticket")) {
+                Cookie cookie = new Cookie("ticket", map.get("ticket").toString());
+                cookie.setPath("/");
+                if (rememberme > 0) {
+                    cookie.setMaxAge(3600*24*5);
+                }
+                response.addCookie(cookie);
+                return ToutiaoUtil.getJSONString(0, "注册成功");
+            } else {
+                return ToutiaoUtil.getJSONString(1, map);
+            }
+*/
+            if(map.isEmpty()){
+                return ToutiaoUtil.getJSONString(0, "注册成功");
+            }else {
+                return ToutiaoUtil.getJSONString(1, map);
+            }
+
+        } catch (Exception e) {
+            //  logger.error("注册异常" + e.getMessage());
+            System.out.println(e.getMessage());
+            return ToutiaoUtil.getJSONString(1, "注册异常");
+        }
+
+    }
+}
--- src/main/java/com/nowcode/toutiao/service/UserService.java
+++ src/main/java/com/nowcode/toutiao/service/UserService.java
@@ -2,13 +2,52 @@ package com.nowcode.toutiao.service;
 
 import com.nowcode.toutiao.dao.UserDAO;
 import com.nowcode.toutiao.model.User;
+import com.nowcode.toutiao.util.ToutiaoUtil;
+import org.apache.tomcat.util.codec.binary.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
 
 @Service
 public class UserService {
     @Autowired
     private UserDAO userDAO;
+
+    //public void addUser(User user){ userDAO.addUser(user);}
+    public Map<String, Object> register(String username, String password){
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        if(username == null||username.length()==0){
+            map.put("msgname", "用户名不能为空");
+            return map;
+        }
+
+        if(password == null||password.length()==0){
+            map.put("messagepassword", "密码不能为空");
+            return map;
+        }
+        User user=userDAO.selectByName(username);
+        if(user!=null) {
+            map.put("messagename", "用户名已经被注册");
+            return map;
+        }
+
+        user = new User();
+        user.setName(username);
+        //user.setPassword(password);
+        user.setSalt(UUID.randomUUID().toString().substring(0, 5));
+        String head = String.format("/head/%dt.png", new Random().nextInt(1000));
+        user.setHeadUrl(head);
+        user.setPassword(ToutiaoUtil.MD5(password+user.getSalt()));
+        userDAO.addUser(user);
+
+        //登陆
+        return map;
+    }
 
     public User getUser(int id){
         return userDAO.selectById(id);
