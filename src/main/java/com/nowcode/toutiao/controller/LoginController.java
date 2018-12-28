6aa97e0d7fc386efb4657cec6a67308b77b37944
billreus <rine328909144@gmail.com>
2018-12-28 22:57:50 +0800

    ticketSQL结束明天DAO层

--- src/main/java/com/nowcode/toutiao/controller/LoginController.java
+++ src/main/java/com/nowcode/toutiao/controller/LoginController.java
@@ -55,4 +55,38 @@ public class LoginController {
         }
 
     }
+
+    @RequestMapping(path = {"/login/"}, method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
+    public String login(Model model, @RequestParam("username") String username,
+                      @RequestParam("password") String password,
+                      @RequestParam(value="rember", defaultValue = "0") int rememberme) {
+        try {
+            Map<String, Object> map = userService.register(username, password);
+/*
+            if (map.containsKey("ticket")) {
+                Cookie cookie = new Cookie("ticket", map.get("ticket").toString());
+                cookie.setPath("/");
+                if (rememberme > 0) {
+                    cookie.setMaxAge(3600*24*5);
+                }
+                response.addCookie(cookie);
+                return ToutiaoUtil.getJSONString(0, "注册成功");
+            } else {
+                return ToutiaoUtil.getJSONString(1, map);
+            }
+*/
+            if(map.isEmpty()){
+                return ToutiaoUtil.getJSONString(0, "注册成功");
+            }else {
+                return ToutiaoUtil.getJSONString(1, map);
+            }
+
+        } catch (Exception e) {
+            //  logger.error("注册异常" + e.getMessage());
+            System.out.println(e.getMessage());
+            return ToutiaoUtil.getJSONString(1, "注册异常");
+        }
+
+    }
 }
--- src/main/java/com/nowcode/toutiao/service/UserService.java
+++ src/main/java/com/nowcode/toutiao/service/UserService.java
@@ -48,6 +48,41 @@ public class UserService {
         return map;
     }
 
+    public Map<String, Object> login(String username, String password){
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        if(username == null||username.length()==0){
+            map.put("msgname", "用户名不能为空");
+            return map;
+        }
+
+        if(password == null||password.length()==0){
+            map.put("messagepassword", "密码不能为空");
+            return map;
+        }
+        User user=userDAO.selectByName(username);
+        if(user == null) {
+            map.put("messagename", "用户名不存在");
+            return map;
+        }
+
+        if(!ToutiaoUtil.MD5(password+user.getSalt()).equals(user.getPassword())){
+            map.put("msgpwd", "密码不正确");
+            return map;
+        }
+        user = new User();
+        user.setName(username);
+        //user.setPassword(password);
+        user.setSalt(UUID.randomUUID().toString().substring(0, 5));
+        String head = String.format("/head/%dt.png", new Random().nextInt(1000));
+        user.setHeadUrl(head);
+        user.setPassword(ToutiaoUtil.MD5(password+user.getSalt()));
+        userDAO.addUser(user);
+
+        //登陆
+        return map;
+    }
+
     public User getUser(int id){
         return userDAO.selectById(id);
     }
